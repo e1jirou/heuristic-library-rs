ee2a0ddf5b61258678303be4ffb28cf24bf64ec9
e1jirou <hir_kitahara@yahoo.co.jp>
2024-07-10 23:37:29 +0900

    [add] fenwick_tree

---
+++ src/fenwick_tree.rs
@@ -0,0 +1,57 @@
+type T = i32;
+
+#[derive(Debug)]
+pub struct FenwickTree {
+    n: usize,
+    data: Vec<T>
+}
+
+impl FenwickTree {
+    pub fn new(n: usize) -> FenwickTree {
+        FenwickTree {
+            n,
+            data: vec![T::default(); n],
+        }
+    }
+
+    pub fn add(&mut self, mut p: usize, x: T) {
+        debug_assert!(p < self.n);
+        p += 1;
+        while p <= self.n {
+            self.data[p - 1] += x;
+            p += p & (!p + 1);
+        }
+    }
+
+    pub fn sum(&self, l: usize, r: usize) -> T {
+        debug_assert!(l <= r && r <= self.n);
+        self.sum_left(r) - self.sum_left(l)
+    }
+
+    pub fn sum_left(&self, mut r: usize) -> T {
+        debug_assert!(r <= self.n);
+        let mut s = 0;
+        while r > 0 {
+            s += self.data[r - 1];
+            r -= r & (!r + 1);
+        }
+        s
+    }
+
+    pub fn bisect(&self, x: T) -> usize {
+        let mut s = 0;
+        let mut p = 0;
+        for i in (0..(64 - self.n.leading_zeros())).rev() {
+            let q = p | (1 << i);
+            if q > self.n {
+                continue;
+            }
+            let t = s + self.data[q - 1];
+            if t < x {
+                s = t;
+                p = q;
+            }
+        }
+        p
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod articulation_point;
 #[allow(unused)]
 pub mod beam_search;
 pub mod erasable_priority_queue;
+pub mod fenwick_tree;
 pub mod geometry;
 pub mod index_set;
 pub mod object_pool;
