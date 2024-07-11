0e335a05486d5d60574cdcf59d1c40122915cc6c
e1jirou <hir_kitahara@yahoo.co.jp>
2024-07-11 11:37:56 +0900

    [update] DoubleEndedPriorityQueue

--- src/erasable_priority_queue.rs
+++ src/erasable_priority_queue.rs
@@ -41,10 +41,64 @@ impl<T: Ord> ErasablePriorityQueue<T> {
         self.data.peek()
     }
 
+    pub fn len(&self) -> usize {
+        self.data.len() - self.lazy.len()
+    }
+
     fn update(&mut self) {
         while !self.lazy.is_empty() && self.data.peek().unwrap() == self.lazy.peek().unwrap() {
             self.data.pop();
             self.lazy.pop();
         }
+    }
+}
+
+pub struct DoubleEndedPriorityQueue<T> {
+    min_queue: ErasablePriorityQueue<std::cmp::Reverse<T>>,
+    max_queue: ErasablePriorityQueue<T>,
+}
+
+impl <T: Copy + Ord> DoubleEndedPriorityQueue<T> {
+    pub fn new() -> Self {
+        DoubleEndedPriorityQueue {
+            min_queue: ErasablePriorityQueue::new(),
+            max_queue: ErasablePriorityQueue::new(),
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        DoubleEndedPriorityQueue {
+            min_queue: ErasablePriorityQueue::with_capacity(capacity),
+            max_queue: ErasablePriorityQueue::with_capacity(capacity),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.min_queue.is_empty()
+    }
+
+    pub fn push(&mut self, item: T) {
+        self.min_queue.push(std::cmp::Reverse(item));
+        self.max_queue.push(item);
+    }
+
+    pub fn erase(&mut self, item: T) {
+        self.min_queue.erase(std::cmp::Reverse(item));
+        self.max_queue.erase(item);
+    }
+
+    pub fn peek_min(&self) -> Option<&T> {
+        match self.min_queue.peek() {
+            Some(ret) => Some(&ret.0),
+            None => None,
+        }
+    }
+
+    pub fn peek_max(&self) -> Option<&T> {
+        self.max_queue.peek()
+    }
+
+    pub fn len(&self) -> usize {
+        self.min_queue.len()
     }
 }
