4df425ce2c424b95804245db85df0b1248d0e899
e1jirou <hir_kitahara@yahoo.co.jp>
2024-07-20 23:52:58 +0900

    [update] combinations with replacement

--- src/heuristic/itertools.rs
+++ src/heuristic/itertools.rs
@@ -65,3 +65,50 @@ impl<T: Clone> Combinations<T> {
         true
     }
 }
+
+pub struct CombinationsWithReplacement<T> {
+    n: usize,
+    r: usize,
+    data: Vec<T>,
+    indices: Vec<usize>,
+    combination: Vec<T>,
+}
+
+impl<T: Clone> CombinationsWithReplacement<T> {
+    pub fn new(data: &[T], r: usize) -> Self {
+        let n = data.len();
+        debug_assert!(r <= n);
+        CombinationsWithReplacement {
+            n,
+            r,
+            data: data.to_vec(),
+            indices: vec![0; r],
+            combination: vec![data[0].clone(); r],
+        }
+    }
+
+    pub fn get_combination(&self) -> &Vec<T> {
+        &self.combination
+    }
+
+    pub fn next_combination(&mut self) -> bool {
+        // backward
+        while !self.indices.is_empty() && *self.indices.last().unwrap() == self.n - 1 {
+            self.indices.pop();
+            self.combination.pop();
+        }
+        let s = self.indices.len();
+        if s == 0 {
+            return false;
+        }
+        // forward
+        let p = self.indices[s - 1] + 1;
+        self.indices.pop();
+        self.combination.pop();
+        for _ in (s - 1)..self.r {
+            self.indices.push(p);
+            self.combination.push(self.data[p].clone());
+        }
+        true
+    }
+}
