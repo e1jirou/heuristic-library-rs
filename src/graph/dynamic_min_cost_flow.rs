f825bb35dadefacad820e1ee5360b2df1d911ace
e1jirou <hir_kitahara@yahoo.co.jp>
2024-07-28 10:53:09 +0900

    [update] delete some members to clone

--- src/graph/dynamic_min_cost_flow.rs
+++ src/graph/dynamic_min_cost_flow.rs
@@ -1,5 +1,3 @@
-use itertools::Itertools;
-
 #[derive(Clone)]
 pub struct Edge<T> {
     pub to: usize,
@@ -9,6 +7,7 @@ pub struct Edge<T> {
     pub is_fwd: bool,
 }
 
+#[derive(Clone)]
 pub struct DynamicMinCostFlow<T> {
     n: usize,
     edges: Vec<Vec<Edge<T>>>,
@@ -16,11 +15,6 @@ pub struct DynamicMinCostFlow<T> {
     dual: Vec<T>,
     supplies: Vec<T>,
     cost: T,
-    dist: Vec<T>,
-    prev_e: Vec<usize>,
-    vis: Vec<bool>,
-    que_min: Vec<usize>,
-    que: std::collections::BinaryHeap<std::cmp::Reverse<(T, usize)>>,
 }
 
 impl<T> DynamicMinCostFlow<T>
@@ -34,11 +28,6 @@ where T: std::fmt::Debug + num_traits::NumAssign + num_traits::PrimInt + std::op
             dual: vec![T::zero(); n],
             supplies: vec![T::zero(); n],
             cost: T::zero(),
-            dist: vec![T::zero(); n],
-            prev_e: vec![0; n],
-            vis: vec![false; n],
-            que_min: Vec::new(),
-            que: std::collections::BinaryHeap::new(),
         }
     }
 
@@ -74,40 +63,37 @@ where T: std::fmt::Debug + num_traits::NumAssign + num_traits::PrimInt + std::op
     // update dual
     fn change_to_non_negative_cost(&mut self, s: usize, cost: T) {
         debug_assert!(cost < T::zero());
+        
+        let mut costs = vec![T::zero(); self.n];
+        let mut que_min = Vec::new();
+        let mut que: std::collections::BinaryHeap<std::cmp::Reverse<(T, usize)>> = std::collections::BinaryHeap::new();
 
-        self.dist.fill(T::zero());
-        self.vis.fill(false);
-        self.que_min.clear();
-        self.que.clear();
+        costs[s] = cost;
+        que_min.push((cost, s));
 
-        self.dist[s] = cost;
-        self.que_min.push(s);
-
-        while !self.que_min.is_empty() || !self.que.is_empty() {
-            let v = if !self.que_min.is_empty() {
-                self.que_min.pop().unwrap()
+        while !que_min.is_empty() || !que.is_empty() {
+            let (cost_v, v) = if !que_min.is_empty() {
+                que_min.pop().unwrap()
             } else {
-                self.que.pop().unwrap().0.1
+                que.pop().unwrap().0
             };
-            if self.vis[v] {
+            if costs[v] < cost_v {
                 continue;
             }
-            self.vis[v] = true;
-            let cost_v = self.dist[v];
             self.dual[v] += cost_v;
             let dual_v = self.dual[v];
             for e in &self.edges[v] {
+                let e = e.clone();
                 if e.cap == T::zero() {
                     continue;
                 }
-                let to = e.to;
-                let cost_to = e.cost - self.dual[to] + dual_v;
-                if cost_to < self.dist[to] {
-                    self.dist[to] = cost_to;
-                    if cost == cost_v {
-                        self.que_min.push(to);
+                let cost_to = e.cost - self.dual[e.to] + dual_v;
+                if cost_to < costs[e.to] {
+                    costs[e.to] = cost_to;
+                    if cost_to == cost_v {
+                        que_min.push((cost_to, e.to));
                     } else {
-                        self.que.push(std::cmp::Reverse((cost_to, to)));
+                        que.push(std::cmp::Reverse((cost_to, e.to)));
                     }
                 }
             }
@@ -180,9 +166,6 @@ where T: std::fmt::Debug + num_traits::NumAssign + num_traits::PrimInt + std::op
             self.edges.push(Vec::new());
             self.dual.push(T::zero());
             self.supplies.push(T::zero());
-            self.dist.push(T::zero());
-            self.prev_e.push(0);
-            self.vis.push(false);
             ret
         }
     }
@@ -206,7 +189,7 @@ where T: std::fmt::Debug + num_traits::NumAssign + num_traits::PrimInt + std::op
 
     // return the amount of the flow
     pub fn flow_with_limits(&mut self, flow_limit: T, cost_limit: T) -> T {
-        let sources = (0..self.n).filter(|&v| self.supplies[v] > T::zero()).collect_vec();
+        let sources: Vec<usize> = (0..self.n).filter(|&v| self.supplies[v] > T::zero()).collect();
         if sources.is_empty() {
             return T::zero();
         }
@@ -214,39 +197,45 @@ where T: std::fmt::Debug + num_traits::NumAssign + num_traits::PrimInt + std::op
         for p in &mut self.dual {
             *p -= min_potential;
         }
+        let mut dist = vec![T::zero(); self.n];
+        let mut prev_e = vec![0; self.n];
+        let mut vis = vec![false; self.n];
+
+        let mut que_min = Vec::new();
+        let mut que = std::collections::BinaryHeap::new();
+
         let mut flow = T::zero();
 
         while flow < flow_limit {
-            self.dist.fill(T::max_value());
-            self.vis.fill(false);
-            self.que_min.clear();
-            self.que.clear();
+            dist.fill(T::max_value());
+            vis.fill(false);
+            que_min.clear();
+            que.clear();
 
             for &s in &sources {
                 if self.supplies[s] > T::zero() {
-                    // source
                     let cost = -self.dual[s];
-                    self.dist[s] = cost;
-                    self.que.push(std::cmp::Reverse((cost, s)));
+                    dist[s] = cost;
+                    que.push(std::cmp::Reverse((cost, s)));
                 }
             }
             let mut t = usize::MAX;
-            while !self.que_min.is_empty() || !self.que.is_empty() {
-                let v = if !self.que_min.is_empty() {
-                    self.que_min.pop().unwrap()
+            while !que_min.is_empty() || !que.is_empty() {
+                let v = if !que_min.is_empty() {
+                    que_min.pop().unwrap()
                 } else {
-                    self.que.pop().unwrap().0.1
+                    que.pop().unwrap().0.1
                 };
-                if self.vis[v] {
+                if vis[v] {
                     continue;
                 }
-                self.vis[v] = true;
+                vis[v] = true;
                 if self.supplies[v] < T::zero() {
                     t = v;
                     break;
                 }
                 let dual_v = self.dual[v];
-                let dist_v = self.dist[v];
+                let dist_v = dist[v];
                 for e in &self.edges[v] {
                     let e = e.clone();
                     if e.cap == T::zero() {
@@ -254,17 +243,17 @@ where T: std::fmt::Debug + num_traits::NumAssign + num_traits::PrimInt + std::op
                     }
                     let cost = e.cost - self.dual[e.to] + dual_v;
                     debug_assert!(cost >= T::zero());
-                    if self.dist[e.to] - dist_v > cost {
+                    if dist[e.to] - dist_v > cost {
                         let dist_to = dist_v + cost;
                         if dist_to > cost_limit {
                             continue;
                         }
-                        self.dist[e.to] = dist_to;
-                        self.prev_e[e.to] = e.rev;
+                        dist[e.to] = dist_to;
+                        prev_e[e.to] = e.rev;
                         if dist_to == dist_v {
-                            self.que_min.push(e.to);
+                            que_min.push(e.to);
                         } else {
-                            self.que.push(std::cmp::Reverse((dist_to, e.to)));
+                            que.push(std::cmp::Reverse((dist_to, e.to)));
                         }
                     }
                 }
@@ -273,16 +262,16 @@ where T: std::fmt::Debug + num_traits::NumAssign + num_traits::PrimInt + std::op
                 break;
             }
             for v in 0..self.n {
-                if !self.vis[v] {
+                if !vis[v] {
                     continue;
                 }
-                self.dual[v] -= self.dist[t] - self.dist[v];
+                self.dual[v] -= dist[t] - dist[v];
             }
 
             let mut c = (flow_limit - flow).min(-self.supplies[t]);
             let mut v = t;
             while self.supplies[v] <= T::zero() {
-                let e = &self.edges[v][self.prev_e[v]];
+                let e = &self.edges[v][prev_e[v]];
                 c = c.min(self.edges[e.to][e.rev].cap);
                 v = e.to;
             }
@@ -290,9 +279,9 @@ where T: std::fmt::Debug + num_traits::NumAssign + num_traits::PrimInt + std::op
             c = c.min(self.supplies[s]);
             v = t;
             while self.supplies[v] <= T::zero() {
-                self.edges[v][self.prev_e[v]].cap += c;
-                let fwd = self.edges[v][self.prev_e[v]].rev;
-                v = self.edges[v][self.prev_e[v]].to;
+                self.edges[v][prev_e[v]].cap += c;
+                let fwd = self.edges[v][prev_e[v]].rev;
+                v = self.edges[v][prev_e[v]].to;
                 self.edges[v][fwd].cap -= c;
             }
             let d = self.dual[t] - self.dual[s];
