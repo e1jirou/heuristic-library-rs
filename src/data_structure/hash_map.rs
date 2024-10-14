055e9f739cb79fe3c139157987a88f4666b97b56
e1jirou <hir_kitahara@yahoo.co.jp>
2024-10-14 18:51:23 +0900

    [update] open addressing hash map

--- src/data_structure/hash_map.rs
+++ src/data_structure/hash_map.rs
@@ -9,7 +9,7 @@ impl<V: Clone> ChainHashMap<V> {
     pub fn new(capacity: usize, seed: u64) -> Self {
         use rand::{Rng, SeedableRng};
         let mut rng = rand_pcg::Mcg128Xsl64::seed_from_u64(seed);
-        let capacity = rng.gen_range((2 * capacity)..(4 * capacity));
+        let capacity = rng.gen_range((1 * capacity)..(2 * capacity));
         let xor = rng.gen_range(0..=usize::MAX);
         Self {
             capacity,
@@ -47,7 +47,7 @@ impl<V: Clone> ChainHashMap<V> {
         let group = self.hash(key);
         match Self::find(key, &self.data[group]) {
             Some(i) => Some(&self.data[group][i].1),
-            None => None
+            None => None,
         }
     }
 
@@ -55,7 +55,7 @@ impl<V: Clone> ChainHashMap<V> {
         let group = self.hash(key);
         match Self::find(key, &self.data[group]) {
             Some(i) => Some(&mut self.data[group][i].1),
-            None => None
+            None => None,
         }
     }
 
@@ -82,6 +82,91 @@ impl<V: Clone> ChainHashMap<V> {
                 Some(self.data[group].swap_remove(i).1)
             }
             None => None,
+        }
+    }
+}
+
+pub struct OpenAddressHashMap<V> {
+    capacity: usize,
+    xor: usize,
+    len: usize,
+    data: Vec<(usize, V)>,
+}
+
+impl<V: Clone + Default> OpenAddressHashMap<V> {
+    pub fn new(capacity: usize, seed: u64) -> Self {
+        use rand::{Rng, SeedableRng};
+        let mut rng = rand_pcg::Mcg128Xsl64::seed_from_u64(seed);
+        let capacity = rng.gen_range((2 * capacity)..(4 * capacity));
+        let xor = rng.gen_range(0..=usize::MAX);
+        Self {
+            capacity,
+            xor,
+            len: 0,
+            data: vec![(usize::MAX, V::default()); capacity],
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn clear(&mut self) {
+        self.data.fill((usize::MAX, V::default()));
+        self.len = 0;
+    }
+
+    fn hash(&self, key: usize) -> usize {
+        (key ^ self.xor) % self.capacity
+    }
+
+    fn find(&self, key: usize, mut i: usize) -> Result<usize, usize> {
+        while self.data[i].0 != key {
+            if self.data[i].0 == usize::MAX {
+                return Err(i);
+            }
+            i += 1;
+            if i == self.capacity {
+                i = 0;
+            }
+        }
+        Ok(i)
+    }
+
+    pub fn contains_key(&self, key: usize) -> bool {
+        let i = self.hash(key);
+        self.find(key, i).is_ok()
+    }
+
+    pub fn get(&self, key: usize) -> Option<&V> {
+        let i = self.hash(key);
+        match self.find(key, i) {
+            Ok(i) => Some(&self.data[i].1),
+            Err(_) => None,
+        }
+    }
+
+    pub fn get_mut(&mut self, key: usize) -> Option<&mut V> {
+        let i = self.hash(key);
+        match self.find(key, i) {
+            Ok(i) => Some(&mut self.data[i].1),
+            Err(_) => None,
+        }
+    }
+
+    pub fn insert(&mut self, key: usize, mut value: V) -> Option<V> {
+        let i = self.hash(key);
+        match self.find(key, i) {
+            Ok(i) => {
+                std::mem::swap(&mut self.data[i].1, &mut value);
+                Some(value)
+            }
+            Err(i) => {
+                self.data[i] = (key, value);
+                self.len += 1;
+                debug_assert_ne!(self.len, self.capacity);
+                None
+            }
         }
     }
 }
